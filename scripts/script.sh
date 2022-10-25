f91c659ff94d8d0717b498ebefccdb2c8ff31dce
mos-security <agencia.seguridad.mos@gmail.com>
2022-10-25 19:11:18 +0200

    ftp ssh y vpn

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
-apt install -y ssh ftp apache2 samba-common mysql-client \
+apt update -y
+
+apt install -y ssh openssh-server openssh-client ftp \
+apache2 samba-common mysql-client vsftpd \
 iptables rsync filezilla isc-dhcp-client \
-php7.4 javascript-common python3
+php7.4 javascript-common python3 openvpn
+
+cd  /home/usuario/Descargas
+
+tar xzvf VPN-MOS-SECURITY-client-AUTORIDAD-MOS-SECURITY.tar.gz
+
+openvpn VPN-MOS-SECURITY-client.conf
