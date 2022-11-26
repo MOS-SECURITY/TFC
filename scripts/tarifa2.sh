7d87b09694363770ef87df5e8a63803a2952bce3
MOS-SECURITY <agencia.seguridad.mos@gmail.com>
2022-11-26 17:41:20 +0100

    tarifas corregidas

--- scripts/tarifa2.sh
+++ scripts/tarifa2.sh
@@ -8,21 +8,22 @@ openvpn
 
 
 echo ""
+echo "Para trabajar con VPN deberás partir siempre desde el escritotio"
+echo ""
 read -p "Hola, escriba el nombre de usuario con el que trabaja: " user
 echo ""
 cd  /home/$user/Descargas
 ruta=$(pwd)
-echo "Para trabajar con VPN deberás partir siempre desde el escritotio"
 echo ""
 echo "Se encuentra en el directorio ${ruta}" 
 
 # DESCOMPRESIÓN Y LOGEO EN VPN DE ZENTYAL
 
-tar xzvf VPN-MOS-SECURITY-client-AUTORIDAD-MOS-SECURITY.tar.gz
+tar xzvf MOS-SECURITY-client-MOS-SECURITY.tar.gz
 mkdir MOS-VPN
-mv VPN-MOS-SECURITY-client.conf 966E8287774297BE.pem AUTORIDAD-MOS-SECURITY.pem cacert.pem /home/$user/Descargas/MOS-VPN
-rm -r VPN-MOS-SECURITY-client-AUTORIDAD-MOS-SECURITY.tar.gz
+mv MOS-SECURITY-client.conf 5D650147FD170F7D.pem MOS-SECURITY.pem cacert.pem /home/$user/Descargas/MOS-VPN
+rm -r MOS-SECURITY-client-MOS-SECURITY.tar.gz
 chmod -R u+x MOS-VPN
 mv MOS-VPN /home/$user/Escritorio
 cd /home/$user/Escritorio/MOS-VPN
-openvpn VPN-MOS-SECURITY-client.conf
+openvpn MOS-SECURITY-client.conf
