c90b2c21ed2719d581616be2f7186ade5918050a
MOS-SECURITY <agencia.seguridad.mos@gmail.com>
2022-11-27 15:25:46 +0100

    tarifas

---
+++ WEB/login/descargas/tarifa2.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+apt update -y
+
+apt install -y samba-common rsync isc-dhcp-client apache2 php7.4 javascript-common python3 openvpn
+
+echo " "
+echo "Para trabajar con VPN deberás partir siempre desde el escritotio"
+echo " "
+read -p "Hola, escriba el nombre de usuario con el que trabaja: " user
+echo " "
+cd  /home/$user/Descargas
+ruta=$(pwd)
+echo " "
+echo "Se encuentra en el directorio ${ruta}"
+
+# DESCOMPRESIÓN Y LOGEO EN VPN DE ZENTYAL
+
+tar xzvf MOS-SECURITY-client-cliente-MOS-SECURITY.tar.gz
+mkdir MOS-VPN
+mv MOS-SECURITY-client.conf 5D7FC5FE1FED3B6A.pem cliente-MOS-SECURITY.pem cacert.pem /home/$user/Descargas/MOS-VPN
+rm -r MOS-SECURITY-client-cliente-MOS-SECURITY.tar.gz
+chmod -R u+x MOS-VPN
+mv MOS-VPN /home/$user/Escritorio
+cd /home/$user/Escritorio/MOS-VPN
+openvpn MOS-SECURITY-client.conf
