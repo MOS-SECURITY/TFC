e8b84b0f05a41c615ffdcb6b05e88c1c4641a0fe
MOS-SECURITY <agencia.seguridad.mos@gmail.com>
2022-12-05 18:29:05 +0100

    videos, tutorial descargas, cara, login err, encriptar

--- WEB/login/registro.php
+++ WEB/login/registro.php
@@ -5,10 +5,10 @@ $usuario=$_POST['usuario'];
 $password=$_POST['password'];
 $correo=$_POST['correo'];
 $tarifa=$_POST['tarifa'];
-
+ 
 // Encriptamos contraseña
 
-//$password = hash('sha512' , $password);
+//$pass_encript = password_hash($password,PASSWORD_DEFAULT);
 
 $insert_into_db = $connect_db->prepare("INSERT INTO registro (usuario,correo,contraseña,tarifa) VALUES ('$usuario','$correo','$password','$tarifa')");
 $insertCorrecto=$insert_into_db ->execute();
