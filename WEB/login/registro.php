cb7d1a0c68c89f54034ba438ab669ee3bb394262
MOS-SECURITY <agencia.seguridad.mos@gmail.com>
2022-12-07 18:15:56 +0100

    DividirPresentacion,cara Miguel,Inyeccion

--- WEB/login/registro.php
+++ WEB/login/registro.php
@@ -6,10 +6,6 @@ $password=$_POST['password'];
 $correo=$_POST['correo'];
 $tarifa=$_POST['tarifa'];
  
-// Encriptamos contraseña
-
-//$pass_encript = password_hash($password,PASSWORD_DEFAULT);
-
 $insert_into_db = $connect_db->prepare("INSERT INTO registro (usuario,correo,contraseña,tarifa) VALUES ('$usuario','$correo','$password','$tarifa')");
 $insertCorrecto=$insert_into_db ->execute();
 
--- WEB/login/validar.php
+++ WEB/login/validar.php
@@ -3,19 +3,13 @@ include('db.php');
 $usuario=$_POST['usuario'];
 $password=$_POST['password'];
 
-// Verificación de contraseña encriptada
-
-//$password = hash('sha512' , $password);
-
 $consulta=$connect_db->query("SELECT * FROM registro where usuario='$usuario'and contraseña='$password'");
 
 foreach($consulta as $rr) {
   $tarifa = $rr['tarifa'];
 };
 
-echo $tarifa;
-
-// $filas=mysqli_num_rows($resultado);
+ echo $tarifa;
 
 if($tarifa == "Servicios Completos"){
   
@@ -30,14 +24,13 @@ if($tarifa == "Servicios Completos"){
   header("location:descargaRemoto.html");
 
 } else {
-  include("index.html")
+  header("location:login.html");
 
   ?>
    <h1 class="bad">Usuario o contraseña incorrectos</h1><br>
    <h3 class="bad">¿No tienes cuenta?</h3>
    <h3 class="bad">¡Regístrate abajo!</h3>
  <?php
-}
-mysqli_free_result($resultado);
+} 
 mysqli_close($connect_db);
 ?>
